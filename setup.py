5bf5230c28928a26cc6f7bdd859fec70d8a3a516
Andrew Zah <zah@andrewzah.com>
2020-10-22 17:07:32 -0400

    add setup.py metadata for installing

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='keycloak_oauth',
+      version='1.0',
+      description='Keycloak OAuth for Jupyterhub',
+      author='Andrew Zah',
+      author_email='zah@andrewzah.com',
+      url='https://github.com/ossys/jupyterhub-keycloak-oidc',
+     )
