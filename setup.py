ab6fa3abedaa2d1dfaeaa201cf37ea3ad7e069e5
Andrew Zah <zah@andrewzah.com>
2020-10-23 12:21:01 -0400

    re-organize directory structure

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(name='keycloakoauth',
-      version='1.0.0',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='keycloakoauth',
+      version='1.0',
       description='Keycloak OAuth for Jupyterhub',
       author='Andrew Zah',
       author_email='zah@andrewzah.com',
       url='https://github.com/ossys/jupyterhub-keycloak-oidc',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=setuptools.find_packages(),
      )
